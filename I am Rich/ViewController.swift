ffd3e37989a95b7d08b0144cb87e856b940cce7e
Prahalad Belavadi <github@belavadi.com>
2020-02-20 19:58:31 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Prahalad on 20/02/20.
+//  Copyright © 2020 Prahalad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
